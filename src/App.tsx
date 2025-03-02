a40d40daf1aba9e16772912033cf963721e602cb
Royce Joe <roylory@gmail.com>
2025-03-02 14:47:47 -0800

    margin top to print button

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container mx-auto p-1 md:p-4">
       <Calendar />
-      <button onClick={() => window.print()} className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-400 print:hidden flex items-center justify-center gap-2 cursor-pointer mx-auto">
+      <button onClick={() => window.print()} className="mt-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-400 print:hidden flex items-center justify-center gap-2 cursor-pointer mx-auto">
         <PrinterIcon className="w-6 h-6" />
         Print
       </button>
