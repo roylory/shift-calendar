825a0b69a9dda7c76ff12999bf8cbbaf992dc5b9
Royce Joe <roylory@gmail.com>
2025-03-02 14:46:37 -0800

    font style update inside cell

--- src/ContentEditable.tsx
+++ src/ContentEditable.tsx
@@ -14,7 +14,7 @@ function ContentEditable({ ampm, day, month, year }: ContentEditableProps) {
       contentEditable
       spellCheck="false"
       autoCorrect="false"
-      className="w-full h-full p-0 text-center resize-none flex justify-center items-center leading-none text-[10px] md:text-xs"
+      className="w-full h-full p-0 text-center resize-none flex justify-center items-center leading-none text-[10px] md:text-xs lg:text-sm overflow-hidden text-ellipsis font-stretch-condensed"
       onBlur={(e) => setText(e.currentTarget.innerHTML)}
       onKeyDown={(e) => {
         if (e.key === "Enter") {
