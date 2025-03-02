55ce20662f81d7d05d41e32af145d18fedf51e7d
Royce Joe <roylory@gmail.com>
2025-03-02 14:40:15 -0800

    less padding in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { PrinterIcon } from "@heroicons/react/20/solid"
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-1 md:p-4">
       <Calendar />
       <button onClick={() => window.print()} className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-400 print:hidden flex items-center justify-center gap-2 cursor-pointer mx-auto">
         <PrinterIcon className="w-6 h-6" />
