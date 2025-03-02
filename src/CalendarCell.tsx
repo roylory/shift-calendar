61183e6f327d2745a1cdef2e685d97f340032592
Royce Joe <roylory@gmail.com>
2025-03-02 14:21:23 -0800

    fix print issue

--- src/CalendarCell.tsx
+++ src/CalendarCell.tsx
@@ -4,10 +4,10 @@ function CalendarCell({ day, month, year }: { day: number, month: number, year: 
   return (
     <div className="flex flex-col p-0.5 relative">
       <span className="absolute left-2 top-1 text-sm text-gray-800">{day}</span>
-      <div className="h-15 bg-amber-200 print:bg-amber-200!">
+      <div className="h-15 bg-amber-200">
         <ContentEditable ampm="am" day={day} month={month} year={year} />
       </div>
-      <div className="h-15 bg-blue-100 print:bg-blue-100!">
+      <div className="h-15 bg-blue-100">
         <ContentEditable ampm="pm" day={day} month={month} year={year} />
       </div>
     </div>
